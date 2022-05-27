4047c4bfcb5f8d0e5fe42633a4782eecd390d97e
KiemNV <kiemnv@kaiyouit.com>
2022-05-27 10:22:38 +0700

    Publishes model

--- src/SimpleLoginServiceProvider.php
+++ src/SimpleLoginServiceProvider.php
@@ -8,6 +8,9 @@ class SimpleLoginServiceProvider extends ServiceProvider
     public function boot() {
         $this->loadRoutesFrom(__DIR__.'/routes/api.php');
         $this->publishes([
+            __DIR__.'/app/Models/' => app_path('Models')
+        ], 'models');
+        $this->publishes([
             __DIR__.'/database/' => database_path()
         ], 'database');
     }
